24e37d459dd52434c7c71ef464d69ccc0567653d
hbhasin <harpreetbhasinphd@gmail.com>
2017-05-11 21:10:22 -0600

    Update Boulder_Daily_Forecast_2016.py

--- Boulder_Daily_Forecast_2016.py
+++ Boulder_Daily_Forecast_2016.py
@@ -5,7 +5,7 @@ import pandas as pd
 import datetime 
 import time
 
-apikey = '9f94f7690008a8c2432d26c83e8f1521'
+apikey = 'enter your key here'
  
 Boulder = [40.014984, -105.270546, 1451631600]
 Boulder_Time = 1451631600
@@ -24,17 +24,13 @@ b_mth = '12/'
 for b_day in range(1,32):
     dt = datetime.datetime(2016, 12, b_day, 0, 0)
     req_list[b_mth + str(b_day) + '/2016'] = int((time.mktime(dt.timetuple())))
-#print(req_list)
 sortedReqList = sorted(req_list.items())
-#for i in sortedReqList:
-#    print(i)
-    
+   
 columns = ['Date', 'cloudCover', 'apparentTemperatureMax', 'apparentTemperatureMin', 'temperatureMax', 'temperatureMin', 'windSpeed', 'humidity', 'visibility', 'time']
 index = ['Row']
 dF = pd.DataFrame(columns = columns, index = index)
 dF1= pd.DataFrame(columns = columns, index = index)
 csv_file = "C:\Temp\Boulder_Dec_2016_Daily_Weather_Forecast.csv"
-#print(type(dF))
 for req_day in sortedReqList:
     Boulder_Time = req_day[1]
     fio = ForecastIO.ForecastIO(apikey,
@@ -44,15 +40,9 @@ for req_day in sortedReqList:
 
     if fio.has_daily() is True:
         daily = FIODaily.FIODaily(fio)
-        #print 'Daily'
-        #print 'Summary:' #, daily.summary
-        #print 'Icon:' #, daily.icon
-        #print
         for day in xrange(0, daily.days()):
-            #print 'Day', day+1
             for item in daily.get_day(day).keys():
                 print item + ' : ' + unicode(daily.get_day(day)[item])
-                #dF['Row'] = day
                 dF['Date'] = req_day[0]
                 if item == 'time':
                     dF[str(item)] = daily.get_day(day)[item]
@@ -73,15 +63,11 @@ for req_day in sortedReqList:
                 if item == 'visibility':
                     dF[str(item)] = daily.get_day(day)[item]
             dF1 = dF1.append(dF)    
-            #print
-        #print
     else:
         print 'No Daily data'
-
-#print dF
 
 #csv_file = "C:\Temp\Jan2015.csv"
 dF1.to_csv(csv_file, sep=',')
 
 new_dF = pd.read_csv(csv_file,index_col=0)
-print new_dF
+
